77d8614416fb6961e7bb2e0fec08b4ef2d050cf6
Bartosz Szadkowski <bartoszszadkowski@MacBook-Air-Bartosz.local>
2020-05-02 04:28:16 +0200

    main functionalities

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,1 +1,15 @@
 # RockPaperScissors
+'''
+1. one game or 3 game
+2. choose from: rock, paper, scissors
+3. player == rock:
+    rock loose with paper
+    rock win with scissors
+4. player == paper
+    paper loose with scissors
+    paper win with rock
+5. player == scissors
+    scissors loose with rock 
+    scissors win with paper
+
+'''
