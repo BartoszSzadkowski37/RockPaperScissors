210bdf6fccca2b9458c2bbab8ec8fd3b20d69efa
Bartosz Szadkowski <bartoszszadkowski@MacBook-Air-Bartosz.local>
2020-05-02 15:10:09 +0200

    additional features

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,7 +1,7 @@
 # RockPaperScissors
 '''
-1. one game or 3 game
-2. choose from: rock, paper, scissors
+1. one game or 3 games - inputMenu 
+2. choose from: rock, paper, scissors - inputMenu
 3. player == rock:
     rock loose with paper
     rock win with scissors
@@ -11,7 +11,13 @@
 5. player == scissors
     scissors loose with rock 
     scissors win with paper
-6. playerWins, aIwins
+6. playerWins, aIwins --> variables 
 7. if 3 games and 2 games the same winner end game
 8. again?
 '''
+
+import random
+import pyinputplus as pyip
+
+choices = ['rock', 'paper', 'scissors']
+
