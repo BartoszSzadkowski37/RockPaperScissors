5a515d0cb2913c0f02dc42ec807a266745f9251c
Bartosz Szadkowski <bartoszszadkowski@MacBook-Air-Bartosz.local>
2020-05-04 15:06:26 +0200

    On three games check TIE option

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -16,8 +16,90 @@
 8. again?
 '''
 
+# Import modules
 import random
 import pyinputplus as pyip
 
+# List of possible choices
 choices = ['rock', 'paper', 'scissors']
 
+# Welcome screen
+print('------------------------------------------------------')
+print('--------------Rock--Paper--And--Scissors--------------')
+print('------------------------------------------------------')
+print('Welcome!')
+
+# Choose how many times player wants to play
+howManyGames = pyip.inputChoice(['one', 'three', '1', '3'], 'How many times would you like to play? (one or three): ')
+
+# Value to main loop, when it will be False, then game is over
+game = True
+
+# This function check who is the winner
+def checkWin(player, aI):
+    winner = ''
+    if player == 'rock':
+        if aI == 'rock':
+            winner = 'tie'
+        elif aI == 'paper':
+            winner = 'aI'
+        else:
+            winner = 'player'
+    elif player == 'paper':
+        if aI == 'paper':
+            winner = 'tie'
+        elif aI == 'scissors':
+            winner = 'aI'
+        else:
+            winner = 'player'
+    else:
+        if aI == 'scissors':
+            winner = 'tie'
+        elif aI == 'rock':
+            winner = 'aI'
+        else:
+            winner = 'player'
+    return winner
+
+# This function is one game, return the winner
+def game():
+
+        print('--------------------------------------------')
+        player = pyip.inputMenu(['rock', 'paper', 'scissors'])
+        aI = choices[random.randint(0,2)]
+        winner = checkWin(player, aI)
+        print('\n\n----------------------------------------------')
+        print('Your choice: %s   vs   My choice: %s' % (player, aI))
+        if winner == 'player':
+            print('You win!')
+        elif winner == 'aI':
+            print('I win!')
+        else:
+            print('Tie!')
+        return winner
+
+# Main Loop
+while game:
+    if howManyGames == 'one' or howManyGames == '1':
+        game()
+    else:
+        playersWin = 0
+        aIwin = 0
+        for i in range(3):
+            winner = game()
+            if winner == 'player':
+                playersWin += 1
+            else:
+                aIwin += 1
+            if playersWin == 2 or aIwin == 2:
+                break
+        print('-----------------------------------------')
+        if playersWin > aIwin:
+            print('You win!')
+        else:
+            print('I win!')
+        print('-----------------------------------------')
+            
+    again = pyip.inputYesNo('Play again?')
+    if again == 'no':
+        game = False
